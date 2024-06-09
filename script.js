064b657113058890a263404597e02e11535c2696
9064abc <163479544+9064abc@users.noreply.github.com>
2024-06-09 22:19:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-var file = new FileReader(); 
+/*var file = new FileReader(); 
 file.readAsText(sample.sharp);
 var ptag = document.getElementById("context");
-ptag.textcontent = file.result;
+ptag.textcontent = file.result;*/
+var ptag = document.getElementById("context");
+var file_url = "sample.sharp";
+fetch(file_url).then((response) => {
+    if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.text();
+}).then((text) => {
+    ptag.textcontent = text;
+}).catch((error) => {
+    ptag.textContent = `Could not fetch verse: ${error}`;
+});
 var s = [];
