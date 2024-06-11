da357869d5de89083e1c6e6abf232549975cd07b
9064abc <163479544+9064abc@users.noreply.github.com>
2024-06-11 16:16:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var ptag = document.getElementById("context");
 ptag.textcontent = file.result;*/
 var ptag = document.getElementById("context");
 var file_url = "sample.sharp";
+var TXT
 fetch(file_url).then((response) => {
     if (!response.ok) {
         throw new Error(`HTTP error: ${response.status}`);
@@ -12,13 +13,14 @@ fetch(file_url).then((response) => {
 }).then((text) => {
     //text = text.replace(/\n/g,"<br>")
     ptag.textContent = text;
+    TXT = text;
 }).catch((error) => {
     ptag.textContent = `Could not fetch verse: ${error}`;
 });
-var s = [];
+var s = {};
 class Node = {
     node_type;
-    children;
+    children = [];
     constructor(node_type,children){
         this.node_type = node_type;
         this.children =children;
@@ -28,9 +30,42 @@ class Type = {
     tag_name;
     attribute;
     text;
-    consrructor(tag_name,attr,text){
+    constructor(tag_name,attr,text){
         this.tag_name = tag_name;
-        this.attribute = attr;
+        //this.attribute = attr;
         this.text = text;
     }
 };
+//text = ptag.textContent;
+var l = TXT.length
+var S,T,attr;
+function tagAttr(i){
+    while(TXT[i] != ">"){
+        S += TXT[i];
+        i+=1;
+    }
+    var l = S.length
+    var j = 0
+    while(S[j]!="{" && j<l){
+        T += S[j];
+        j += 1;
+    }
+    j += 1
+    while(S[j]!="}" && j<l){
+        attr += S[j]
+        j += 1;
+    }
+    console.log("text  "+T);
+    console.log("attr  "+attr);
+    return i+1
+}
+while(i < l){
+    if(TXT[i] == "<"){
+        /*i+=1;
+        while(TXT[i] != ">"){
+            S += TXT[i];
+            i+=1;
+        }*/
+        i = tagAttr(i+1);
+    } 
+}
