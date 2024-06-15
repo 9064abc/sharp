9e7945abff6c753b994c96c241db9b52e2083033
9064abc <163479544+9064abc@users.noreply.github.com>
2024-06-15 16:23:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var file_url = "sample.sharp";
 var TXT;
 var ln = -1;
 var l;
+var nodes = [];
 fetch(file_url).then((response) => {
     if (!response.ok) {
         throw new Error(`HTTP error: ${response.status}`);
@@ -29,8 +30,10 @@ class Node{
         this.node_type = node_type;
         //this.children.push(children);
     }
-    Nchild(child){
-        this.children.push(child);
+    Nchildren(ch){
+        for(var child of ch){
+            this.children.push(child);
+        }
     }
 };
 class Type{
@@ -44,18 +47,26 @@ class Type{
     }
 };
 //text = ptag.textContent;
-function perse_text(i){
+function perse_text(i,name,attr){
     var j = i;
     var S = "";
+    var r = 0;
+    var ch = [];
     while(TXT[j] != "#"){
         if(TXT[j] == "<"){
-            j = tagAttr(j+1);
+            r = tagAttr(j+1);
+            j = r[0];
+            ch.push(r[1]);
         }else{
             S += TXT[j];
             j += 1
         }
     }
     console.log("text  "+S);
+    var type = new Type(name,attr,S);
+    //ch = ["head","body"]
+    var dom = new Node(type,ch);
+    nodes.push(dom);
     return j+1;
 }
 
@@ -63,18 +74,22 @@ function tagAttr(i){
     var S = "";
     var T = "";
     var attr = "";
-    var j = 0
+    var j = 0;
+    var r = 0;
     while(TXT[i] != ">"){
         S += TXT[i];
         i+=1;
     }
     l = S.length
     
-    while(S[j]!="{" && j<l){
+    while(S[j]!=" " && j<l){
         T += S[j];
         j += 1;
     }
-    j += 1
+    while(S[j]!="{" && j<l){
+        j += 1
+    }
+    j += 1;
     while(S[j]!="}" && j<l){
         attr += S[j]
         j += 1;
@@ -84,13 +99,17 @@ function tagAttr(i){
     }
     console.log("tag_name  "+T);
     console.log("attr  "+attr);
-    return i+1
+    i = perse_text(i+1,T,attr);
+    r = [i+1,T];
+    return r
 }
 function perse(){
     i=0
     var type = new Type("sharp","","parent");
-    var dom = new Node(type,0);
-    console.log(dom);
+    var ch = ["head","body"]
+    var dom = new Node(type,ch);
+    nodes.push(dom);
+    console.log(nodes);
     while(i < ln){
         if(TXT[i] == "<"){
             /*i+=1;
@@ -98,9 +117,9 @@ function perse(){
                 S += TXT[i];
                 i+=1;
             }*/
-            i = tagAttr(i+1);
+            i = tagAttr(i+1)[0];
             
-            i = perse_text(i);
+            //i = perse_text(i);
         //}else if(TXT[i] =="#"){
             //i += 1
             //i = perse_text(i);
@@ -108,4 +127,5 @@ function perse(){
             i += 1;
         } 
     }
+    console.log(nodes);
 }
